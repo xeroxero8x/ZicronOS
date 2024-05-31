aa91ecaa95642fb1d9902a895f523a260a739cc4
John Doe <example@gmail.com>
2024-05-31 21:43:49 +0600

    nvim update

--- config/home/files.nix
+++ config/home/files.nix
@@ -9,7 +9,7 @@
   home.file.".config/rofi/rofi.jpg".source = ./files/rofi.jpg;
   home.file.".config/swaylock-bg.jpg".source = ./files/media/swaylock-bg.jpg;
   home.file.".config/ascii-neofetch".source = ./files/ascii-neofetch;
-  home.file.".config/nvim".source = ./files/nvim;
+#  home.file.".config/nvim".source = ./files/nvim;
   home.file.".config/hyprlock-bg.png".source = ./files/media/hyprlock-bg.png;
   home.file.".local/share/fonts" = {
     source = ./files/fonts;
